ace5c3060a43b482a5b185754b970401df6fabd2
Ivan Angelov <ivanangelov414@gmail.com>
2025-09-24 17:23:49 +0300

    Extracted Identity Migrations management into its own Tools project and script

--- src/Tools/Identity/Program.cs
+++ src/Tools/Identity/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddPersistence(builder.Configuration);
-builder.Services.AddDomainServices();
+builder.Services.AddIdentity(builder.Configuration);
 
 if (args.Contains("--migrate"))
 {
