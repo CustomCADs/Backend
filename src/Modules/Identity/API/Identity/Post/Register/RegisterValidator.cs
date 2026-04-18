7187b6c6056f2b6985dfb8ff2163e928a7156616
Ivan Angelov <ivanangelov414@gmail.com>
2026-04-18 16:40:11 +0300

    Strengthened Register Validator

---
+++ src/Modules/Identity/API/Identity/Post/Register/RegisterValidator.cs
@@ -0,0 +1,38 @@
+﻿using CustomCADs.Modules.Identity.Application.Contracts;
+using CustomCADs.Shared.Application;
+using CustomCADs.Shared.Domain;
+using FluentValidation;
+
+namespace CustomCADs.Modules.Identity.API.Identity.Post.Register;
+
+using static ApplicationConstants.FluentMessages;
+using static DomainConstants;
+
+public class RegisterRequestValidator : Validator<RegisterRequest>
+{
+	public RegisterRequestValidator()
+	{
+		RuleFor(x => x.Role)
+			.Must(x => x is Roles.Customer or Roles.Contributor)
+			.WithMessage("""Role must be either "Customer" or "Contributor" """);
+
+
+		RuleFor(x => x.Username)
+			.MustAsync(
+				async (username, ct) => !await Resolve<IUserService>()
+					.GetExistsByUsernameAsync(username)
+					.ConfigureAwait(false)
+			).WithMessage("Cannot register a User with a Duplicate Username");
+
+		RuleFor(x => x.Email)
+			.MustAsync(
+				async (email, ct) => !await Resolve<IUserService>()
+					.GetExistsByEmailAsync(email)
+					.ConfigureAwait(false)
+			).WithMessage("Cannot register a User with a Duplicate Email");
+
+		RuleFor(x => x.ConfirmPassword)
+			.NotEmpty().WithMessage(RequiredError)
+			.Equal(x => x.Password).WithMessage("Passwords must be equal!");
+	}
+}
