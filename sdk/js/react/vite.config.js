49df1fbdaddd7dbfa9dd936df1708c3c1fa559a7
Ivan Angelov <ivanangelov414@gmail.com>
2026-05-13 15:13:56 +0300

    Updated packages

--- sdk/js/react/vite.config.js
+++ sdk/js/react/vite.config.js
@@ -1,16 +1,9 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-import alias from '@rollup/plugin-alias';
-import path from 'path';
 
 const config = defineConfig({
 	plugins: [
-		alias({
-			entries: [
-				{ find: '@', replacement: path.resolve(__dirname, 'src') },
-			],
-		}),
 		viteTsConfigPaths({
 			projects: ['./tsconfig.json'],
 		}),
