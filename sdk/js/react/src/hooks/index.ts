35939b66d3184ddfd54091a561f65f823739083d
Ivan Angelov <ivanangelov414@gmail.com>
2026-05-13 16:45:14 +0300

    SDK `queryCall` fn -> `query` object

--- sdk/js/react/src/hooks/index.ts
+++ sdk/js/react/src/hooks/index.ts
@@ -1,6 +1,6 @@
 export * as queries from './queries';
 export { useQuery } from './queries/useQuery';
 export { useInfiniteQuery } from './queries/useInfiniteQuery';
-export { queryCall } from './queries/queryCall';
+export { query } from './queries/query';
 export * as mutations from './mutations';
 export { useMutation } from './mutations/useMutation';
